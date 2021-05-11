541fe1f93ad7b34880db9abcb1b20097e31eb420
LAPTOP-L8E5G43E\Nikki <kimberlyretherford@icloud.com>
2021-05-10 20:40:57 -0400

    installed Mvvmhelpers. Changed home page view from listview to collectionview.

--- Open_Hands/ViewModels/HomeViewModel.cs
+++ Open_Hands/ViewModels/HomeViewModel.cs
@@ -1,4 +1,5 @@
-﻿using Open_Hands.Models;
+﻿using MvvmHelpers;
+using Open_Hands.Models;
 using Open_Hands.Persistence;
 using Open_Hands.Views;
 using System;
@@ -13,14 +14,20 @@ namespace Open_Hands.ViewModels
         public Command CreateEventCommand { get; }
         public IEventDetails _eventDetails;
         public EventDetails EventDetails { get; set; }
-        public IEnumerable<EventDetails> Events { get; set; }
+        public ObservableRangeCollection<EventDetails> Events { get; set; }
         public HomeViewModel()
         {
             CreateEventCommand = new Command(OnCreateEventClicked);
             _eventDetails = new EventDetailsRepository();
-            var Events = _eventDetails.GetEvents();
+            Events = new ObservableRangeCollection<EventDetails>();
+            DisplayEvents();
         }
 
+        public async void DisplayEvents()
+        {
+            var events = await _eventDetails.GetEvents();
+            Events.AddRange(events);
+        }
         private async void OnCreateEventClicked(object obj)
         {
             await Shell.Current.GoToAsync($"//{nameof(CreateEventPage)}");
